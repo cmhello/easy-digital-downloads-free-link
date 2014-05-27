defb6d8c7bae21f79ac3d1fb038e45e21a63bcd8
webaware <webaware@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-27 22:30:03 +0000

    1.0.1 move settings into EDD settings page > Extensions
    git-svn-id: https://plugins.svn.wordpress.org/easy-digital-downloads-free-link/trunk@921985 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class.EddFreeLinkPlugin.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+* class for managing the plugin
+*/
+class EddFreeLinkPlugin {
+
+	/**
+	* static method for getting the instance of this singleton object
+	* @return self
+	*/
+	public static function getInstance() {
+		static $instance = null;
+
+		if (is_null($instance)) {
+			$instance = new self();
+		}
+
+		return $instance;
+	}
+
+	/**
+	* hook actions and filters
+	*/
+	private function __construct() {
+		add_filter('init', array($this, 'init'));
+		add_filter('plugin_row_meta', array($this, 'addPluginDetailsLinks'), 10, 2);
+
+		// Easy Digital Download hooks
+		add_filter('edd_purchase_download_form', array($this, 'eddPurchaseDownloadForm'), 20, 2);
+		add_filter('edd_settings_extensions', array($this, 'eddSettingsExtensions'));
+
+		// Shopfront theme hooks
+		add_filter('shopfront_purchase_download_form', array($this, 'shopfrontPurchaseLink'), 20, 2);
+	}
+
+	/**
+	* init action
+	*/
+	public function init() {
+		load_plugin_textdomain('edd-free-link', false, basename(dirname(EDD_FREE_LINK_PLUGIN_FILE)) . '/languages/');
+	}
+
+	/**
+	* settings items
+	* @param array $settings
+	* @return array
+	*/
+	public function eddSettingsExtensions($settings) {
+		$our_settings = array(
+			array(
+				'id' => 'edd_free_link_header',
+				'name' => sprintf('<strong>%s</strong>', __('EDD Free Link', 'edd-free-link')),
+				'desc' => '',
+				'type' => 'header',
+				'size' => 'regular'
+			),
+			array(
+				'id' => EDD_FREE_LINK_OPT_LINK_LABEL,
+				'name' => __( 'Download link label', 'edd' ),
+				'desc' => '',
+				'type' => 'text',
+				'size' => 'regular'
+			),
+		);
+
+		return array_merge($settings, $our_settings);
+	}
+
+	/**
+	* action hook for adding plugin details links
+	*/
+	public function addPluginDetailsLinks($links, $file) {
+		if ($file == DISABLE_EMAILS_PLUGIN_NAME) {
+			$links[] = '<a href="http://wordpress.org/support/plugin/easy-digital-downloads-free-link">' . __('Get help', 'edd-free-link') . '</a>';
+			$links[] = '<a href="http://wordpress.org/plugins/easy-digital-downloads-free-link/">' . __('Rating', 'edd-free-link') . '</a>';
+			$links[] = '<a href="http://shop.webaware.com.au/easy-digital-downloads-free-link/">' . __('Donate', 'edd-free-link') . '</a>';
+		}
+
+		return $links;
+	}
+
+	/**
+	* intercept download form, replace with a link if product is free and only has one download file
+	* @param string $purchase_form
+	* @param array $args
+	* @return string
+	*/
+	public function eddPurchaseDownloadForm($purchase_form, $args) {
+		global $edd_options;
+
+		$download_id = absint($args['download_id']);
+		if ($download_id) {
+			$price = floatval(edd_get_lowest_price_option($args['download_id']));
+
+			if ($price < 0.001) {
+				$files = edd_get_download_files($download_id);
+
+				if (count($files) == 1 && !empty($files[0]['file'])) {
+					$download_url = $files[0]['file'];
+					$download_label = empty($edd_options[EDD_FREE_LINK_OPT_LINK_LABEL]) ? __('Download', 'edd-free-link') : $edd_options[EDD_FREE_LINK_OPT_LINK_LABEL];
+					$download_link_classes = implode(' ', array('edd_free_link', $args['style'], $args['color'], trim($args['class'])));
+					$template = empty($args['edd_free_link_icon']) ? 'download-link' : 'download-icon';
+
+					$download_label = apply_filters('edd_free_link_label', $download_label, $download_id, $args);
+					$template = apply_filters('edd_free_link_template', $template, $download_id, $args);
+
+					// build download link
+					ob_start();
+					$this->loadTemplate($template, compact('download_url', 'download_label', 'download_link_classes', 'args'));
+					$purchase_form = ob_get_clean();
+				}
+			}
+		}
+
+		return $purchase_form;
+	}
+
+	/**
+	* intercept Shopfront theme purchase link and maybe replace with download link
+	* @param string $purchase_form
+	* @param array $args
+	* @return string
+	*/
+	public function shopfrontPurchaseLink($purchase_form, $args) {
+		$args['edd_free_link_icon'] = 'icon-product';
+		return $this->eddPurchaseDownloadForm($purchase_form, $args);
+	}
+
+	/**
+	* load template from theme or plugin
+	* @param string $template name of template to load
+	* @param array $templateData data to make available to templates
+	*/
+	protected function loadTemplate($template, $templateData = array()) {
+		global $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
+		global $woocommerce, $product;
+
+		extract($templateData);
+
+		$templatePath = locate_template("plugins/easy-digital-downloads-free-link/$template.php");
+		if (!$templatePath) {
+			$templatePath = EDD_FREE_LINK_PLUGIN_ROOT . "templates/$template.php";
+		}
+
+		require $templatePath;
+	}
+
+}
---
+++ readme.txt
@@ -0,0 +1,58 @@
+=== Easy Digital Downloads Free Link ===
+Contributors: webaware
+Plugin Name: Easy Digital Downloads Free Link
+Plugin URI: http://shop.webaware.com.au/easy-digital-downloads-free-link/
+Author URI: http://webaware.com.au/
+Donate link: http://shop.webaware.com.au/easy-digital-downloads-free-link/
+Tags: download, downloads, digital downloads, easy digital downloads
+Requires at least: 3.7
+Tested up to: 3.9.1
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+replace EDD add-to-cart button with download link when product is free
+
+== Description ==
+
+Replace the add-to-cart button in Easy Digital Downloads with a direct link to the download file when the product is free and has only one file. This only works when the file is an external link to a web-accessible file.
+
+For archive pages on the [Shop Front theme](http://wordpress.org/themes/shop-front), the cart icon is replaced with a download icon and direct download link.
+
+Want to see it in action? Check out [my shop](http://shop.webaware.com.au/) -- it's running this plugin.
+
+== Installation ==
+
+1. Either install automatically through the WordPress admin, or download the .zip file, unzip to a folder, and upload the folder to your /wp-content/plugins/ directory. Read [Installing Plugins](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins) in the WordPress Codex for details.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+= Why? =
+
+Because I wanted to add my free plugins to an Easy Digital Downloads shop, without making it harder to download them from the shop than from WordPress.org plugin pages.
+
+= Are downloads logged in Easy Digital Download? =
+
+No. Downloads are direct from the site hosting the linked file.
+
+= Can I change the download link label? =
+
+Yes, see Downloads > Settings > Extensions. There's also a filter hook, `edd_free_link_label`, if you want finer control over the link label.
+
+= Can I change the download link HTML? =
+
+Yes, there are templates. Look in the templates folder of the plugin. There's also a filter hook, `edd_free_link_template`, if you want finer control over which template is used.
+
+== Screenshots ==
+
+1. Download button on a free download
+2. Download icon on Shop Front theme home page
+
+== Changelog ==
+
+= 1.0.1 [2014-05-28] =
+* change: move settings into EDD settings page > Extensions
+
+= 1.0.0 [2014-05-26] =
+* initial public version
